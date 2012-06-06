825349c737164b658c38a6580025cac632ff66a9
Scott Kirkland <srkirkland@gmail.com>
2012-06-06 22:37:38 +0000

    using ejs as the new view engine

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ var app = module.exports = express.createServer();
 
 app.configure(function(){
   //app.set('views', __dirname + '/views'); //=> default
-  app.set('view engine', 'jade');
+  //app.set('view engine', 'jade');
+  app.set('view engine', 'ejs');
+  app.set('view options', {layout: false});
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   //app.use(require('stylus').middleware({ src: __dirname + '/public' }));
