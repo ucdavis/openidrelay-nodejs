6092f143f007f1e2c4c28e943760efcfe4830ae5
Scott Kirkland <srkirkland@gmail.com>
2012-06-07 21:09:58 +0000

    added openid provider switcher

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,7 @@
-/* Author:
-
-*/
-
-
-
-
-
-
-
+/* Author: Scott Kirkland */
+$(function() {
+    $(".provider").click(function(e) {
+        e.preventDefault();
+        $("#openid_identifier").val(this.href);
+    });
+});
