c392c6101c46876ff7292949d0a1eded730ea212
Scott Kirkland <srkirkland@gmail.com>
2012-06-07 20:59:10 +0000

    full member's only login support

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -10,18 +9,20 @@ var app = module.exports = express.createServer();
 
 // Configuration
 
-app.configure(function(){
-  //app.set('views', __dirname + '/views'); //=> default
-  //app.set('view engine', 'jade');
-  app.set('view engine', 'ejs');
-  //app.set('view options', {layout: false});
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: "1234" }));
-  //app.use(require('stylus').middleware({ src: __dirname + '/public' }));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public')); //=> static files under public
+app.configure(function() {
+    //app.set('views', __dirname + '/views'); //=> default
+    //app.set('view engine', 'jade');
+    app.set('view engine', 'ejs');
+    //app.set('view options', {layout: false});
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(express.session({
+        secret: "1234"
+    }));
+    //app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public')); //=> static files under public
 });
 
 /*
@@ -35,37 +36,55 @@ app.configure('production', function(){
 */
 
 // Routes
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Home',
-    authenticated: req.session.auth,
-    username: req.session.authname
-  });
+app.get('/', function(req, res) {
+    res.render('index', {
+        title: 'Home',
+        authenticated: req.session.auth,
+        username: req.session.authname
+    });
 });
 
-app.get('/login', function(req,res){
-  res.render('login', {title: 'Login'});  
+app.get('/login', function(req, res) {
+    res.render('login', {
+        title: 'Login'
+    });
+});
+
+app.get('/logout', function(req, res) {
+    req.session.auth = false;
+    res.writeHead(302, {
+        Location: '/'
+    });
+    res.end();
+});
+
+app.get('/membersonly', function(req, res) {
+    if (req.session.auth) {
+        res.render('membersonly', {
+            title: "Member's Only",
+            username: req.session.authname
+        });
+    }
+    else {
+        res.writeHead(302, {
+            Location: '/login'
+        });
+        res.end();
+    }
+
+});
+
+app.get('/about', function(req,res){
+   res.render('about', {title: 'About'}); 
 });
 
 app.get('/authenticate', openidmethods.authenticate);
 
 app.get('/verify', openidmethods.verify);
 
-app.get('/about', function(req, res){
-  res.render('about', {
-    title: 'About'
-  });
-});
-
-app.get('/contact', function(req, res){
-  res.render('contact', {
-    title: 'Contact'
-  });
-});
-
 // Only listen on $ node app.js
 
 if (!module.parent) {
-  app.listen(process.env.PORT);
-  console.log("Express server listening on port %d", app.address().port);
+    app.listen(process.env.PORT);
+    console.log("Express server listening on port %d", app.address().port);
 }
